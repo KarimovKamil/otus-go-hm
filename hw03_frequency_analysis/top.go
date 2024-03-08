460f686d30c6821c6e54e67a49ef711f26b325a7
Kamil.Karimov <imanymuch@gmail.com>
2024-03-08 14:47:43 +0300

    Изменения по третьей домашке (частотный анализ).

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,42 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type WordInfo struct {
+	Word      string
+	Frequency int
+}
+
+func Top10(text string) []string {
+	wordFreq := make(map[string]int)
+	words := strings.Fields(text)
+
+	for _, word := range words {
+		wordFreq[word]++
+	}
+
+	wordFreqSlice := make([]WordInfo, 0, len(wordFreq))
+	for k, v := range wordFreq {
+		wordFreqSlice = append(wordFreqSlice, WordInfo{k, v})
+	}
+
+	sort.Slice(wordFreqSlice, func(i, j int) bool {
+		return compareWords(wordFreqSlice[i], wordFreqSlice[j])
+	})
+
+	result := make([]string, 0)
+	for i := 0; i < 10 && i < len(wordFreqSlice); i++ {
+		result = append(result, wordFreqSlice[i].Word)
+	}
+	return result
+}
+
+func compareWords(word1, word2 WordInfo) bool {
+	if word1.Frequency == word2.Frequency {
+		return strings.Compare(word1.Word, word2.Word) < 0
+	}
+	return word1.Frequency > word2.Frequency
 }
